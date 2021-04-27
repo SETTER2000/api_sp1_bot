9b6eb85a35ac95387534a50c9b1a5cc81a7b3242
SETTER <info@lphp.ru>
2021-04-27 19:35:37 +0300

    Review first

---
+++ advanced_value.py
@@ -0,0 +1,8 @@
+BOT_ERROR = 'Bot encountered an error'
+TIME_SLEEP = 60 * 20
+DATE_BEGIN = 86400 * 30
+VERDICT = {'reject': 'К сожалению в работе нашлись ошибки.',
+           'reviewing': 'Работа взята в ревью.',
+           'default': 'Ревьюеру всё понравилось, можно приступать к '
+                      'следующему уроку.'
+           }
--- homework.py
+++ homework.py
@@ -1,33 +1,45 @@
+import logging
 import os
-import time
-
 import requests
 import telegram
+import time
+
+
 from dotenv import load_dotenv
+from . import advanced_value as av
 
 load_dotenv()
 
-PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
+
+PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 URL_DOMASHKA = os.getenv('URL_DOMASHKA')
 CHAT_ID = os.getenv('CHAT_ID')
+LOG_NAME_FILE = os.getenv('LOG_NAME_FILE')
 bot_client = telegram.Bot(token=TELEGRAM_TOKEN)
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
+    filename=LOG_NAME_FILE,
+    filemode='w')
 
 
 def parse_homework_status(homework):
+    """Спарсить данные объекта ревью."""
     homework_name = homework['homework_name']
     if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
+        verdict = av.VERDICT['rejected']
     elif homework['status'] == 'reviewing':
-        verdict = 'Работа взята в ревью.'
+        verdict = av.VERDICT['reviewing']
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = av.VERDICT['default']
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
+    """Получить данные ревью."""
     data = {
-        'from_date': current_timestamp - 86400 * 30
+        'from_date': current_timestamp - av.DATE_BEGIN
     }
     homework_statuses = requests.get(
         URL_DOMASHKA,
@@ -37,12 +49,14 @@ def get_homework_statuses(current_timestamp):
 
 
 def send_message(message):
-    #...
+    """Отправить сообщение в Телеграм."""
+    logging.info('Send message')
     return bot_client.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    # проинициализировать бота здесь
+    """Бот по опросу состояния ревью."""
+    logging.debug('Start bot')
     current_timestamp = int(time.time())
 
     while True:
@@ -53,11 +67,13 @@ def main():
                     parse_homework_status(new_homework.get('homeworks')[0]))
             current_timestamp = new_homework.get('current_date',
                                                  current_timestamp)
-            time.sleep(60*20)
+            time.sleep(av.TIME_SLEEP)
 
         except Exception as e:
-            print(f'Бот столкнулся с ошибкой: {e}')
-            time.sleep(5)
+            logging.error(f'{av.BOT_ERROR}: {e}')
+            send_message(f'{av.BOT_ERROR}: {e}')
+            print(f'{av.BOT_ERROR}: {e}')
+            time.sleep(av.TIME_SLEEP)
 
 
 if __name__ == '__main__':
