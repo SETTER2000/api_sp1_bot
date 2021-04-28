cc2900abf06a9ef06321ba599979edf35e7995ac
SETTER <info@lphp.ru>
2021-04-28 18:17:03 +0300

    Review two

--- advanced_value.py
+++ advanced_value.py
@@ -4,6 +4,5 @@ DATE_BEGIN = 86400 * 30
 URL_DOMASHKA = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 VERDICT = {'rejected': 'К сожалению в работе нашлись ошибки.',
            'reviewing': 'Работа взята в ревью.',
-           'default': 'Ревьюеру всё понравилось, можно приступать к '
-                      'следующему уроку.'
-           }
+           'approved': 'Ревьюеру всё понравилось, можно приступать к '
+                       'следующему уроку.'}
--- homework.py
+++ homework.py
@@ -9,41 +9,44 @@ from dotenv import load_dotenv
 import advanced_value as av
 
 load_dotenv()
+try:
+    PRAKTIKUM_TOKEN = os.environ.get('PRAKTIKUM_TOKEN')
+    TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+    CHAT_ID = os.environ.get('CHAT_ID')
+except Exception as e:
+    logging.error(f'{av.BOT_ERROR}: {e}')
 
-PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('CHAT_ID')
-LOG_NAME_FILE = os.getenv('LOG_NAME_FILE')
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
-    filename=LOG_NAME_FILE,
+    filename='main.log',
     filemode='w')
 
 
 def parse_homework_status(homework):
     """Спарсить данные объекта ревью."""
-    homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
-        verdict = av.VERDICT['rejected']
-    elif homework['status'] == 'reviewing':
-        verdict = av.VERDICT['reviewing']
-    else:
-        verdict = av.VERDICT['default']
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    try:
+        verdict = av.VERDICT.get(homework.get('status'))
+        homework_name = homework.get('homework_name')
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    except Exception as e:
+        logging.error(f'{av.BOT_ERROR}: {e}')
 
 
 def get_homework_statuses(current_timestamp):
     """Получить данные ревью."""
-    data = {
-        'from_date': current_timestamp
-    }
-    homework_statuses = requests.get(
-        av.URL_DOMASHKA,
-        params=data,
-        headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'})
-    return homework_statuses.json()
+    try:
+        data = {'from_date': current_timestamp - 8600 * 30}
+        headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+        homework_statuses = requests.get(
+            av.URL_DOMASHKA,
+            params=data,
+            headers=headers)
+        return homework_statuses.json()
+    except Exception as e:
+        logging.error(f'{av.BOT_ERROR}: {e}')
+        return {}
 
 
 def send_message(message, bot_client):
@@ -54,6 +57,7 @@ def send_message(message, bot_client):
 
 def main():
     """Бот по опросу состояния ревью."""
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     logging.debug('Start bot')
     current_timestamp = int(time.time())
 
@@ -71,7 +75,6 @@ def main():
         except Exception as e:
             logging.error(f'{av.BOT_ERROR}: {e}')
             send_message(f'{av.BOT_ERROR}: {e}', bot)
-            print(f'{av.BOT_ERROR}: {e}')
             time.sleep(av.TIME_SLEEP)
 
 
